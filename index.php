c28b4b5c6bf1dd67aa4d940220eaf2c41492fda2
Yamil <yamillazzari@gmail.com>
2020-08-02 18:42:07 -0300

    Base CV

---
+++ index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV - Yamil Ruscalleda</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style type="text/css">
+        body{
+            background-color: #f6f6f6;
+            margin-top: 25px;
+        }
+    </style>
+</head>
+<body>
+
+
+            
+     <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+
+            <div class="card shadow text-center" style="width: 16rem;">
+                <img src="photo.jpg" class="card-img-top shadow" alt="Foto de perfil">
+                <div class="card-body">
+                    <h5 class="btn btn-primary">Yamil Ruscalleda</h5>
+                    <p class="card-text"><strong>Cuil:</strong> 23-37715139-9</p>
+                    <p class="card-text"><strong>Dni:</strong> 37715139</p>
+                    <p class="card-text">16 de Mayo 1994</p>
+                </div>
+            </div>
+            <br>
+            <div class="card shadow text-center" style="width: 16rem;">
+                <div class="card-body">
+                    <a href="https://www.google.com/maps/place/Del+Campo+4339,+S2001JIA+Rosario,+Santa+Fe/" target="_blank" class="btn btn-outline-info">Psj del Campo 4339</a><br><br>
+                    <a href="https://api.whatsapp.com/send?phone=3413203877" target="_blank" class="btn btn-outline-success">(341) 153203877</a><br><br>
+                    <a href="mailto:yamillazzari@gmail.com" target="_blank" class="btn btn-outline-danger">Yamillazzari@gmail.com</a><br>
+                </div>
+            </div>
+            <br>
+            <div class="card shadow text-center" style="width: 16rem;">
+                <div class="card-body">
+                    <p class="card-text"><strong>Estado Civil:</strong> Concubino</p>
+                    <p class="card-text"><strong>Disponibilidad:</strong> Full-Time</p>
+                    <p class="card-text"><strong>Carnet de conducir:</strong> Clase B</p>  
+                </div>
+            </div>
+            <br>
+            <div class="card shadow text-center" style="width: 16rem;">
+                <div class="card-body">
+                    <p class="btn btn-secondary"><strong>Idiomas</strong></p>
+                    <p class="card-text"><strong>Ingles Oral:</strong> Basico</p>
+                    <p class="card-text"><strong>Ingles Escrito:</strong> Intermedio</p>
+                    <p class="card-text"><strong>Español:</strong> Nativo</p>
+                </div>
+            </div>
+            <br>
+            <div class="card shadow text-center" style="width: 16rem;">
+                <div class="card-body">
+                <a href="https://github.com/YamilEzequiel" target="_blank"><img src="image/github.png" ></a>
+                <a href="https://www.facebook.com/YamilLazzari/" target="_blank"><img src="image/facebook.png" ></a>
+                <a href="https://www.linkedin.com/in/yamil-ruscalleda-619a1915b/" target="_blank"><img src="image/linkedin.png" ></a>
+                </div>
+            </div><br>
+            <div class="card shadow text-center" style="width: 16rem;">
+                <div class="card-body">
+                    <p class="btn btn-secondary"><strong>CV</strong></p>
+                    <p class="card-text">Realizado con HTML5 + Bootstrap</p>
+                </div>
+            </div>
+            <br><br>
+
+            </div><!--Finish Col-->
+
+            <div class="col-sm-9 ">    
+
+            <div class="card shadow">
+                <div class="card-body">
+                    <p class="btn btn-secondary"><strong>Experiencia Laboral</strong></p><br>
+
+                    <ul>
+                        <li>       
+                         <strong>2016-Actual</strong><br>
+                        Tecnico Revisador telefónico de Telecom Argentina localidad Rosario
+                        </li><br>
+
+                        <li>       
+                         <strong>2013-2016</strong><br>
+                         Repositor, preparación de pedidos, carga y descarga 
+                         de vehículos (Camiones Chatas), repartidor. Distribui
+                         dora Pampa SRL Av Uriburu 2345  Ref: (0341)6181468
+                        </li><br>
+
+                        <li>       
+                         <strong>2012-2013</strong><br>
+                         Ayudante soldador y montaje estructurales 
+                         en galpones, albañilería en general, instalaciones electricas
+                         e instalaciones de durlock . Ref: (0341)5144199
+                        </li><br>
+
+                        
+                        <li>       
+                         <strong>2010-2012 </strong><br>
+                         Taller Mecánico G.O.L Servicios, Ref: (0341)5018664
+                        </li>
+
+                    </ul>
+                </div>
+            </div><br>
+
+            <div class="card shadow">
+                <div class="card-body">
+                    <p class="btn btn-secondary"><strong>Formación Académica</strong></p><br>
+
+                    <ul>
+                        <li>       
+                         <strong> 2007-2011</strong><br>
+                         Polimodal modalidad Económica y Gestión de las 
+                         Organizaciones Esc. Media Part Leonardo Da Vinci - Rosario
+                        </li><br>
+
+                        <li>       
+                         <strong>2012</strong><br>
+                         Mecánica Automotriz – Cursado y aprobado 1º Año 
+                         ITA – Inst. Tecnológico Avanzado – Rosario
+                        </li><br>
+
+                        <li>       
+                         <strong>2012</strong><br>
+                         Electricidad automotriz – Finalizado y Aprobado .
+                         ITA – Inst. Tecnológico Avanzado – Rosario.
+                        </li><br>
+
+                        
+                        <li>       
+                         <strong>2014 </strong><br>
+                         Electricidad básica y Revisor telefónico Sitratel – Telecom 
+                        </li><br>
+
+                        <li>       
+                         <strong>2018 </strong><br>
+                         Curso Android Studio - Instituto IT Rosario 
+                        </li><br>
+
+                        <li>       
+                         <strong>2009-Actual </strong><br>
+                         Cursos Online y documentaciones de web oficiales sobre programacion, aprendizaje
+                         autodidacta. Principalmente en lenguajes como Java,C#,PHP utilizando programas
+                         como AndroidStudio,Unity y gestion de base de datos MySql.
+                        </li><br>
+
+                    </ul>
+                </div>
+            </div><br>
+
+            <div class="card shadow">
+                <div class="card-body">
+                    <p class="btn btn-secondary"><strong>Conocimientos Informáticos</strong></p><br>
+
+                    <ul>
+                        <li>       
+                         <strong> Lenguajes de programación:</strong><br>
+                         Php, Java, C#, JavaScript, Android+=(Java)
+                        </li><br>
+
+                        <li>       
+                         <strong>Etiquetas y diseño web:</strong><br>
+                         Html5, Css3.
+                        </li><br>
+
+                        <li>       
+                         <strong>Base de datos:</strong><br>
+                         MariaDb, Mysql, manejo con (PhpMyAdmin, Consola), WHM.
+                        </li><br>
+
+                        
+                        <li>       
+                         <strong>Otros , Framework: </strong><br>
+                         jQuery, AJAX, JSON. Composer, Npm, Node, Bootstrap, Laravel, WP, MyBB.
+                        </li><br>
+
+                        <li>       
+                         <strong>Programas: </strong><br>
+                         Eclipse, Android Studio, Dreamweaver, Photoshop, PhotoScape,
+                            FlashFXP, PsMonitor.SublimeText, Unity5, Cinema4D, GameMarker, Visual 
+                            Studio, Git Bash, XAMPP, Microsoft Office, Filezzila , Blender, GitHub.
+                        </li><br>
+
+                        <li>       
+                         <strong>Servicios Web: </strong><br>
+                         Redes, Google ( AdSense ,AdWords, Analytics, Tools, PlayConsole,
+                        PlayStore, Posicionamiento, MarketingWeb), GitHub.
+                        </li><br>
+
+                    </ul>
+                </div>
+            </div><br><br>
+
+            </div><!--Finish Col-->
+         </div><!--Finish Row-->
+    </div><!--Finish Container-->
+
+</body>
+<footer>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</footer>
+
+</html>
